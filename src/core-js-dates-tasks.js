b9ae11e36774a2c4a27508a45a971d4d515c2be8
ntsh <106670334+tffl@users.noreply.github.com>
2024-03-24 12:36:51 +0300

    feat: getDayName solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -49,8 +49,9 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const options = { weekday: 'long' };
+  return new Intl.DateTimeFormat('en-US', options).format(new Date(date));
 }
 
 /**
